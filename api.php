eb5578e0e650e3792ad09c62d63517d697763cbb
JMartinUSA <waverider.jmart@gmail.com>
2016-04-24 14:15:16 -0400

    Added files via upload

---
+++ api.php
@@ -0,0 +1,50 @@
+<?php
+header('Access-Control-Allow-Origin: *'); // Should work in Cross Domain ajax Calling request
+
+mysql_connect("localhost","jaymartm_public","PUBLIC");
+mysql_select_db("jaymartm_Demo");
+
+if(isset($_GET['type']))
+{
+	if($_GET['type'] == "login")
+	{
+		$username = $_GET['UserName'];
+		$password = $_GET['Password'];
+
+		//Create Query
+		$query = "Select * from registration Where UserName='$username' and Password='$password'";
+		//Fire your Query against database
+		$result1 = mysql_query($query);
+		//get total no of rows from database according to the query
+		$totalRows = mysql_num_rows($result1);
+
+		//Prepare Code for json format
+		if($totalRows > 0)
+		{
+			$recipes = array();
+			while($recipe = mysql_fetch_array($result1, MYSQL_ASSOC))
+			{
+				$recipes[] = array('User'=>$recipe);
+			}
+
+			$output = json_encode(array('Users' => $recipes));
+
+			//$output = "{LoginStatus:[{ Success:true, SuccessCode:200 }]}";
+
+			echo $output;
+		}
+
+	}
+	else if($_GET['type'] == 'register')
+	{
+
+	}
+}
+else
+{
+	echo 'Invalid format';
+}
+
+
+
+?>
